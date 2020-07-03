de8486a7e2f33534f00550c511609f5a2c5cc5af
songzhw <songzhw2012@gmail.com>
2020-07-02 21:48:26 -0400

    add log

--- app/src/main/java/ca/six/demo/MainActivity.kt
+++ app/src/main/java/ca/six/demo/MainActivity.kt
@@ -2,10 +2,17 @@ package ca.six.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ca.six.log.L
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvMain.setOnClickListener {
+            println("szw click")
+            L.d("hello SixLog")
+        }
     }
 }
---
+++ lib/src/main/java/ca/six/log/L.java
@@ -0,0 +1,41 @@
+package ca.six.log;
+
+
+import android.util.Log;
+
+/**
+ * Created by songzhw on 4/13/16.
+ */
+public class L {
+    public static String tag = "szw";
+
+    public static void d(String msg){
+        StackTraceElement[] traces = Thread.currentThread().getStackTrace();
+        if(traces == null || traces.length < 8){
+            Log.d(tag,"(normal) "+msg);
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        StackTraceElement element = traces[3];
+
+        String classWholeName = element.getClassName();
+        String className = classWholeName.substring(classWholeName.lastIndexOf(".") + 1);
+
+
+//        for(StackTraceElement element : traces) {
+        sb.append(className);
+        sb.append(" || ");
+        sb.append(element.getMethodName());
+        sb.append("() || ( ");
+        sb.append(className);
+        sb.append(".java:");
+        sb.append(element.getLineNumber());
+        sb.append(")\n");
+//        }
+        sb.append(msg);
+
+        Log.d(tag, sb.toString());
+    }
+}
+
